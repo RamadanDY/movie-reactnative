843c043e03903169f34c8ea9ecf158fb6b7bf575
RamDev <daudalaramadan2@gmail.com>
2025-03-14 11:48:22 +0000

    feat: update index screen layout and add onboarding link; extend Tailwind CSS theme colors

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text className="flex-1">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl color-dark-200">Welcome</Text>
+      <Link href={"/onboarding"}>Onboarding</Link>
     </View>
   );
 }
