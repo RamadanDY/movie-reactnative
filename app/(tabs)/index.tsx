b1119b3478543cdee0f6410c353400cf788d9260
RamDev <daudalaramadan2@gmail.com>
2025-04-01 04:21:10 +0000

    feat: enhance ScrollView component to hide vertical scroll indicator and improve layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,12 @@ export default function Index() {
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
       {/* the scrollview makes the content fit inside the screen no mater the size by making the screen scrollable  */}
-      <ScrollView className="flex-1 px-5  ">
+      {/* also to remove the vertical scroll bar we us an additional prop called show..... and conta....  */}
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", padding: 10 }}
+      >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
       </ScrollView>
       {/* <Text className="text-7xl color-white">helloS</Text> */}
