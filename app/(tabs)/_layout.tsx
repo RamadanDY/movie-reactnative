3c7e8b93dfdc0db5b3afa79f3a5c6357a53ed7d2
RamDev <daudalaramadan2@gmail.com>
2025-04-01 03:10:28 +0000

    feat: enhance tab icon component for improved focused state and styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,22 +7,50 @@ import { icons } from "@/constants/icons";
 // lets create a new component to save the ui of the tab
 // how to make a custom component reusable and to do that we need to pass the props
 const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4  justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font0semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  }
   return (
-    <ImageBackground
-      source={images.highlight}
-      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4  justify-center items-center rounded-full overflow-hidden"
-    >
-      <Image source={icon} tintColor="#151312" className="size-5" />
-      <Text className="text-secondary text-base font0semibold ml-2">
-        {title}
-      </Text>
-    </ImageBackground>
+    <View className="size- full justify-center items-center mt-4 rounded-full">
+      <Image className="size-5" source={icon} tintColor="#A8b5db" />
+    </View>
   );
 };
 
 const Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0d23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 53,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0d23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
