0dcc26a34717c462bd7c4dd6ecde7be41de914b4
RamDev <daudalaramadan2@gmail.com>
2025-04-03 23:34:24 +0000

    feat: add API configuration and environment variables for TMDB integration

---
+++ services/api.ts
@@ -0,0 +1,24 @@
+// TMDB = THE MOVIE DATA DATABASE 
+
+export const TMDB_CONFIG = {
+    
+}
+
+
+
+
+
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDAwODZkZDVkZTk2MGM4N2JhMjQxZWE4NmIzNTZmNCIsIm5iZiI6MTc0MzcxNzc5OS40MDUsInN1YiI6IjY3ZWYwNWE3YTkzMTNjNzE4NGFjZThmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FGABK8_U7XPtakQz4jeURMFyhCR3O9P8dvRKDBPgWYw'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
